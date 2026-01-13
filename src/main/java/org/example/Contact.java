1409cc117e1031208c7c3eefbe60b0fc5b029752
ELEZAR-77 <omarabdullaev68@gmail.com>
2026-01-13 16:34:28 +0300

    Вроде готово

---
+++ src/main/java/org/example/Contact.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Contact {
+    private static Long idCounter = 0L;
+    private Long id;
+    private final String name;
+    private final String phone;
+    private final String email;
+    private final String group;
+
+    public Contact(String name, String phone, String email, String group) {
+        idCounter++;
+        this.id = idCounter;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.group = group;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || this.getClass() != object.getClass()) return false;
+
+        Contact other = (Contact) object;
+
+        return ((Objects.equals(name, other.name)) && (Objects.equals(phone, other.phone)))
+                && (Objects.equals(email, other.email));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 11;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", group='" + group + '\'' +
+                '}';
+    }
+}
